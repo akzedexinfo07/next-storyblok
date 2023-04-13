42b5cb68afad8326f714d6411e6611f90e4a1716
AkshayKhot07 <akshaykhot743@gmail.com>
2023-04-14 00:08:47 +0530

    new changes added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 import Feature from "../components/Feature";
-import Grid from "../components/Grid"
-import Page from "../components/Page"
-import Teaser from "../components/Teaser"
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+import Hero from "../components/Hero";
 
 const components = {
   feature: Feature,
   grid: Grid,
   teaser: Teaser,
   page: Page,
-}
-
+  hero: Hero,
+};
 
 storyblokInit({
   accessToken: "ZabrhqPTVruPDs1NY5ZJ8gtt",
@@ -21,5 +22,5 @@ storyblokInit({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
